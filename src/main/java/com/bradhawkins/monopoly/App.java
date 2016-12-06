6dc36720ca52c33090c5030108a21c34c86a33ee
Brad Hawkins <Brad Hawkins>
2016-12-05 22:29:21 -0700

    Initial Commit

---
+++ src/main/java/com/bradhawkins/monopoly/App.java
@@ -0,0 +1,12 @@
+package com.bradhawkins.monopoly;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) {
+        System.out.println("Hello Monopoly!");
+    }
+}
